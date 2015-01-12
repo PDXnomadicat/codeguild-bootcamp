ba60d19c317c4f241ad4e8ce4e25a09612af4357
Katherine Rea <PDXnomadicat@gmail.com>
2015-01-12 11:51:50 -0800

    Basic version of address book

---
+++ address_book.py
@@ -0,0 +1,70 @@
+
+address_book = {}
+def main():
+
+
+
+	# Main is an entry point and defines a function. Multiple functions and be defined within a file.
+
+	while True:
+
+		# While True: Creates a loop. Loop must eventually close. 
+
+		choice = raw_input("What do you want to do? add entry, remove entry, show list, or exit?  ")               
+
+		# Choice MUST be tabbed - tab after each colon.
+
+		if choice == "add entry":
+
+			name = raw_input("What is the name of the person you would like to add?"  )
+
+			number = raw_input( "What is the number of the person you would like to add?"  ) 
+
+			address_book[name] = number
+
+			print name + " added to list."
+
+		elif choice == "remove entry":
+
+			removed_entry = raw_input("Which entry would you like to remove?")
+
+			del address_book[removed_entry]
+
+			print removed_entry + " Removed from list."
+
+		elif choice == "show list":
+
+			for key in sorted (address_book.keys()):
+
+				print key, address_book[key]
+
+				
+
+		elif choice == "exit":
+
+			print "Thanks for using this program!"
+
+			break
+
+		else: 
+
+			print "I don't understand that command." 
+
+		
+
+
+
+
+
+
+
+
+main()
+ 
+
+
+
+
+
+
+
